23c0734cb2175e61cca1b9eecb91b69804790f40
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2018-08-28 16:53:09 +0000

    Added cors support to our proxy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const GitHub = require('github-api');
 const Promise = require('bluebird');
+const cors = require('cors')
+
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const gh = new GitHub({
